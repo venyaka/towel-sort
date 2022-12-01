e7c6c5e0cca5af1589248de9c22d7bfe187dcc28
Veniamin <112535019+Veniamin-Noise@users.noreply.github.com>
2022-12-01 20:09:01 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function towelSort(massive) {
+    result = []
 
+    for (let i = 0; i < massive.length; i++){
+        if(i % 2 == 0) result.push(massive[i].toString())
+        else result.push(massive[i].reverse().toString())
+    }
+    return result
+}
