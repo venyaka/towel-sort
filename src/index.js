75d0f14573487062252584c896ae26220e9ec382
Veniamin <112535019+Veniamin-Noise@users.noreply.github.com>
2022-12-03 21:25:12 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-function towelSort(massive) {
-    result = []
+function check(str, bracketsConfig) {
 
-    for (let i = 0; i < massive.length; i++){
-        if(i % 2 == 0) result.push(massive[i].toString())
-        else result.push(massive[i].reverse().toString())
+    for (let i = 0; i < bracketsConfig.length; i++) {               // realisation '())('
+        if (str.includes(bracketsConfig[i][0] + bracketsConfig[i][1])) {
+            let clearStr = str.replace([bracketsConfig[i][0] + bracketsConfig[i][1]], '')
+            if (clearStr[0] == '}' || clearStr[0] == ')' || clearStr[0] == ']' || clearStr[-1] == '{' || clearStr[-1] == '(' || clearStr[-1] == '[' || str.length % 2 != 0) {
+                return false
+            }
+        }
+        else {
+            let clearStr2 = str                                     // realisation '([{}])':
+            for (let b = 0; b <= str.length; b++) {
+                if (bracketsConfig.length == 2) {
+                    clearStr2 = clearStr2.replace([bracketsConfig[0][0] + bracketsConfig[0][1]], '').replace([bracketsConfig[1][0] + bracketsConfig[1][1]], '')
+                }
+                else if (bracketsConfig.length == 3) {
+                    clearStr2 = clearStr2.replace([bracketsConfig[0][0] + bracketsConfig[0][1]], '').replace([bracketsConfig[1][0] + bracketsConfig[1][1]], '').replace([bracketsConfig[2][0] + bracketsConfig[2][1]], '')
+                }
+                if (clearStr2 == '') return true
+            }
+            return false
+        }
+        return true
     }
-    return result
+    return true
 }
