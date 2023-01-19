26e770380bfbb0da203564c5162b4c0fc341a30c
Veniamin <112535019+Veniamin-Noise@users.noreply.github.com>
2023-01-19 21:12:34 +0600

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ module.exports = function towelSort(matrix) {
     let result = []
 
     for (let i = 0; i < matrix.length; i++){
-        if(i % 2 == 0) for (let a = 0; a < matrix[i].length; a++) result.push(matrix[i][a])
-        else for (let a = 0; a < matrix[i].length; a++) result.push(matrix[i].reverse()[a])
+        if (i % 2 == 0) 
+        {
+            console.log(matrix[i])
+            for (let a = 0; a < matrix[i].length; a++) result.push(matrix[i][a])
+        }
+        else 
+        {
+            const matrixReverse = matrix[i].reverse()
+            console.log(matrixReverse)
+            for (let a = 0; a < matrix[i].length; a++) result.push(matrixReverse[a])
+        }
     }
     return result
 }
