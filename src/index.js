96168193af6f392ad8a2763627140e9b07969715
Veniamin <112535019+Veniamin-Noise@users.noreply.github.com>
2023-01-19 21:57:25 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-module.exports = function towelSort(matrix = []) {
+module.exports = function towelSort(matrix) {
     let result = []
+
+    if (!matrix) return []
 
     for (let i = 0; i < matrix.length; i++){
         if (i % 2 == 0) 
